060164a463b1d269c42453ebbf273821b607df00
Jalil <abdullacelil00@gmail.com>
2024-10-20 09:11:27 +0400

    Design with CSS styles
    - Install Bulma and import in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
+import 'bulma/css/bulma.min.css';
 import './App.css';
 import {Course} from "./Course.jsx";
+import Angular from '../public/images/channels4_profile.jpg';
+import Bootstrap from '../public/images/Bootstrap_logo.svg.png';
+import FullStack from '../public/images/Link Sharing-Foundation Full Stack Web Development.jpg';
+import FrontEnd from '../public/images/op-Ten-Front-End-Design-Rules-For-Developers_Luke-Social.png';
 
 const App = () => {
     return (
         <>
-            <Course title='Angular' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+            <section className="hero is-link">
+                <div className="hero-body">
+                    <p className="title">
+                        My courses
+                    </p>
+                </div>
+            </section>
+            <div className='columns container section'>
+                <Course title='Angular' image={Angular} description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
             cupiditate dicta dolore, eius eligendi eveniet fugit illum labore mollitia odio perferendis praesentium
             reprehenderit, repudiandae rerum sit soluta tempore. Ex, rerum?'/>
-            <Course title='Bootstrap 5' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                <Course title='Bootstrap 5' image={Bootstrap} description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
             cupiditate dicta dolore, eius eligendi eveniet fugit illum labore mollitia odio perferendis praesentium
             reprehenderit, repudiandae rerum sit soluta tempore. Ex, rerum?'/>
-            <Course title='Full Stack' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                <Course title='Full Stack' image={FullStack} description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
             cupiditate dicta dolore, eius eligendi eveniet fugit illum labore mollitia odio perferendis praesentium
             reprehenderit, repudiandae rerum sit soluta tempore. Ex, rerum?'/>
-            <Course title='Front-end' description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+                <Course title='Front-end' image={FrontEnd} description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
             cupiditate dicta dolore, eius eligendi eveniet fugit illum labore mollitia odio perferendis praesentium
             reprehenderit, repudiandae rerum sit soluta tempore. Ex, rerum?'/>
+            </div>
         </>
     )
 }
--- src/Course.jsx
+++ src/Course.jsx
@@ -1,15 +1,26 @@
-export const Course = ({title, description}) => {
-    /*const title = props.title;
-    const description = props.description;*/
-    // const {title, description} = props;
+export const Course = ({title, image, description}) => {
     return (
-        <>
-            <div>
-                {title}
+        <div className='column'>
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-4by3">
+                        <img src={image} alt="" style={{objectFit: 'cover'}}/>
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media">
+                        <div className="media-content">
+                            <p className="title is-4">
+                                {title}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        {description}
+                    </div>
+                </div>
             </div>
-            <div>
-                {description}
-            </div>
-        </>
+        </div>
     )
 }
